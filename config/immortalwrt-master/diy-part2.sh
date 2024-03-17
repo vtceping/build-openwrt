5b941e14cc6822314debf030bf9bb68448c19854
vtceping <62824521+vtceping@users.noreply.github.com>
2024-03-17 18:01:55 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -28,5 +28,31 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
-# ------------------------------- Other ends -------------------------------
 
+# 最大连接数修改为65535
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
+
+# 替换Passwall为smartdns版
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf package/feeds/luci/luci-app-passwall
+git clone  --depth=1 -b luci-smartdns-dev --single-branch https://github.com/xiaorouji/openwrt-passwall.git package/passwall_luci
+git clone  --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall_packages
+
+# 替换immortalwrt 软件仓库smartdns版本为官方最新版
+rm -rf feeds/packages/net/smartdns
+cp -rf ${GITHUB_WORKSPACE}/patch/smartdns feeds/packages/net
+git clone --depth=1 https://github.com/pymumu/luci-app-smartdns.git package/smartdns_luci
+
+# pushd package/emortal/
+# rm -rf luci-app-omcproxy
+# git clone -b 18.06 https://github.com/lwb1978/luci-app-omcproxy.git
+# popd
+
+# 替换udpxy为修改版
+rm -rf feeds/packages/net/udpxy/Makefile
+cp -rf ${GITHUB_WORKSPACE}/patch/udpxy/Makefile feeds/packages/net/udpxy/
+
+# 卸载酸酸乳
+# ./scripts/feeds uninstall luci-app-ssr-plus
+
+# ------------------------------- Other ends -------------------------------
