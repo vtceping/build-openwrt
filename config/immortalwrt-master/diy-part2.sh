7cf5cb54567e3aec33cfeb743ee70fcd0c4409b2
vtceping <62824521+vtceping@users.noreply.github.com>
2024-03-18 17:54:12 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -44,8 +44,8 @@ cp -rf ${GITHUB_WORKSPACE}/patch/smartdns feeds/packages/net
 git clone --depth=1 https://github.com/pymumu/luci-app-smartdns.git package/smartdns_luci
 
 # golang 1.22
-rm -rf feeds/packages/lang/golang
-git clone --depth=1 https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+# rm -rf feeds/packages/lang/golang
+# git clone --depth=1 https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 
 # pushd package/emortal/
 # rm -rf luci-app-omcproxy
