61d6cccff578c02f4c807bac701802daa43b8198
vtceping <62824521+vtceping@users.noreply.github.com>
2024-04-06 16:31:52 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -7,7 +7,7 @@
 #========================================================================================================================
 
 # mv -f neihe/Makefile ./target/linux/x86/Makefile
-sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
+# sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
 # sed -i 's/KERNEL_TESTING_PATCHVER:=6.6/KERNEL_TESTING_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
 
 # Add a feed source
