98e739bfd9e0bea9e5d299de952cff76967678a5
vtceping <62824521+vtceping@users.noreply.github.com>
2024-10-16 18:07:12 +0800

    Update diy-part1.sh

--- config/Lienol-master/diy-part1.sh
+++ config/Lienol-master/diy-part1.sh
@@ -14,7 +14,7 @@
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '2i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-# sed -i '$a src-git smpackage https://github.com/linhahaz/small-package' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/linhahaz/small-package' feeds.conf.default
 
 # other
 # rm -rf package/utils/{ucode,fbtest}
